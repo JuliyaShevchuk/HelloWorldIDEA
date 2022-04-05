608ddf9557db57bb6841a0061b64f00db8b8a234
JuliyaShevchuk <ju_shevchuk@ukr.net>
2022-04-05 18:14:47 +0300

    commit 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,9 @@ package main.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
+        System.out.println("Hello world IDEA");
+        System.out.println("Hello world IDEA");
         System.out.println("Hello world IDEA");
     }
 }
